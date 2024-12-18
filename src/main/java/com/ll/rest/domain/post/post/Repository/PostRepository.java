e5cda529c8a2571f7dc258d4b380202a37c0d8d6
seeyeon <gulf72pr@swu.ac.kr>
2024-12-18 11:42:15 +0900

    Post Repository

---
+++ src/main/java/com/ll/rest/domain/post/post/Repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.ll.rest.domain.post.post.Repository;
+
+import com.ll.rest.domain.post.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+
+}
