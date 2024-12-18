67a2598de3050b0389eafd867a062a074817cdf2
seeyeon <gulf72pr@swu.ac.kr>
2024-12-18 11:48:04 +0900

    Post,  make sample data

--- src/main/java/com/ll/rest/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/rest/domain/post/post/service/PostService.java
@@ -1,6 +1,7 @@
 package com.ll.rest.domain.post.post.service;
 
 import com.ll.rest.domain.post.post.Repository.PostRepository;
+import com.ll.rest.domain.post.post.entity.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PostService  {
     private final PostRepository postRepository;
+
+    public long count(){
+        return postRepository.count();
+    }
+
+    public Post write(String title, String content){
+        Post post = Post.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+        return postRepository.save(post);
+    }
 }
