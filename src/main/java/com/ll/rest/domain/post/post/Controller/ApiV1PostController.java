73f9fc312b5492282dfc4872061a4f0df8993464
seeyeon <gulf72pr@swu.ac.kr>
2024-12-18 15:12:14 +0900

    use Vaild

--- src/main/java/com/ll/rest/domain/post/post/Controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/Controller/ApiV1PostController.java
@@ -4,7 +4,10 @@ import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
 import com.ll.rest.global.jpa.entity.BaseTime;
 import com.ll.rest.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,13 +38,14 @@ public class ApiV1PostController extends BaseTime {
         return new RsData("200-1", "%d번 글을 삭제했습니다.".formatted(id));
     }
 
-    record PostModifyReqBody(String title, String content){
-
+    record PostModifyReqBody(
+            @NotBlank @Length(min = 2) String title,
+            @NotBlank @Length(min = 2) String content){
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public RsData modifyItem(@PathVariable long id, @RequestBody PostModifyReqBody reqBody){
+    public RsData modifyItem(@PathVariable long id, @RequestBody @Valid PostModifyReqBody reqBody){
         Post post = postService.findById(id).get();
 
         postService.modify(post, reqBody.title, reqBody.content);
