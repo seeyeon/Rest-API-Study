d8d03c45e0a44193b3e042f6a619a8dc28125823
seeyeon <gulf72pr@swu.ac.kr>
2024-12-19 10:55:39 +0900

    DTO 1

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -8,15 +8,15 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto{
     private long id;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+    private LocalDateTime createdDatetime;
+    private LocalDateTime modifiedDatetime;
     private String titile;
     private String content;
 
     public PostDto(Post post) {
         this.id = post.getId();
-        this.createDate = post.getCreatedAt();
-        this.modifyDate = post.getModifiedAt();
+        this.createdDatetime = post.getCreateDate();
+        this.modifiedDatetime = post.getModifiedDate();
         this.titile = post.getTitle();
         this.content = post.getContent();
     }
