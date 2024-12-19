5cdeeeb4807186225e5f664d8e9d0dfe7436a171
seeyeon <gulf72pr@swu.ac.kr>
2024-12-19 11:02:51 +0900

    DTO 2

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.ll.rest.domain.post.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ll.rest.domain.post.post.entity.Post;
 import lombok.Getter;
 
@@ -8,16 +9,29 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto{
     private long id;
-    private LocalDateTime createdDatetime;
-    private LocalDateTime modifiedDatetime;
+    @JsonIgnore
+    private LocalDateTime createDate;
+    @JsonIgnore
+    private LocalDateTime modifyDate;
     private String titile;
     private String content;
 
     public PostDto(Post post) {
         this.id = post.getId();
-        this.createdDatetime = post.getCreateDate();
-        this.modifiedDatetime = post.getModifiedDate();
+        this.createDate = post.getCreateDate();
+        this.modifyDate = post.getModifiedDate();
         this.titile = post.getTitle();
         this.content = post.getContent();
     }
+
+    public LocalDateTime getCreatedDateTime(){
+        return createDate;
+    }
+
+    public LocalDateTime getModifiedDateTime(){
+        return modifyDate;
+    }
+
+
+
 }
