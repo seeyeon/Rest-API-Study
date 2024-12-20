0bb19d2dd5831fe34c472f017abf077902dce1ff
seeyeon <gulf72pr@swu.ac.kr>
2024-12-20 14:45:19 +0900

    PostDto has a MemberDto

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -1,6 +1,7 @@
 package com.ll.rest.domain.post.post.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ll.rest.domain.member.member.dto.MemberDto;
 import com.ll.rest.domain.post.post.entity.Post;
 import lombok.Getter;
 
@@ -13,6 +14,8 @@ public class PostDto{
     private LocalDateTime createDate;
     @JsonProperty("modifiedDatetime")
     private LocalDateTime modifyDate;
+
+    private MemberDto author;
     private String titile;
     private String content;
 
@@ -20,6 +23,8 @@ public class PostDto{
         this.id = post.getId();
         this.createDate = post.getCreateDate();
         this.modifyDate = post.getModifiedDate();
+
+        this.author=new MemberDto(post.getAuthor());
         this.titile = post.getTitle();
         this.content = post.getContent();
     }
